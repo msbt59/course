ade31d7ed2b0c0fbbc6e16145b5cb7d03ec83585
Mohamed Sebabti <momo.rcm59@gmail.com>
2026-06-23 21:35:42 +0200

    1er commit

---
+++ airbnb/models/dim/dim_hosts_cleanesed.sql
@@ -0,0 +1,13 @@
+with scr_hosts as (
+    select * from {{ ref('scr_hosts') }}
+)
+select 
+    HOST_ID,
+    NVL(
+        HOST_NAME,
+       'Anonymous'
+    ) as HOST_NAME,
+    is_superhost,
+    CREATED_AT,
+    UPDATED_AT
+from scr_hosts
