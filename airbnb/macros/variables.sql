8ca68e71a230adceeccc20a71117ebd5583963ba
Mohamed Sebabti <momo.rcm59@gmail.com>
2026-07-23 22:53:09 +0200

    20260723

---
+++ airbnb/macros/variables.sql
@@ -0,0 +1,6 @@
+{% macro learn_variables()%}
+    {% set your_name_jinja = "Mohamed" %}
+    {{ log("Your name is: " ~ your_name_jinja, info=True) }}
+
+    {{ log("Hello dbt user " ~ var("user_name","NO USER NAME IS SET!!!!!!") ~ "!" , info=True) }}
+{% endmacro%}
--- airbnb/models/fct/fct_reviews.sql
+++ airbnb/models/fct/fct_reviews.sql
@@ -12,5 +12,12 @@ SELECT   {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'revi
   * FROM src_reviews
 WHERE review_text is not null
 {% if is_incremental() %}
-  AND review_date > (select max(review_date) from {{ this }})
-{% endif %}
+  {% if var("start_date", False) and var("end_date", False) %}
+    {{ log('Loading ' ~ this ~ ' incrementally (start_date: ' ~ var("start_date") ~ ', end_date: ' ~ var("end_date") ~ ')', info=True) }}
+    AND review_date >= '{{ var("start_date") }}'
+    AND review_date < '{{ var("end_date") }}'
+  {% else %}
+    AND review_date > (select max(review_date) from {{ this }})
+    {{ log('Loading ' ~ this ~ ' incrementally (all missing dates)', info=True)}}
+  {% endif %}
+{% endif %} 
